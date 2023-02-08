5c1c90f5e75ccfee3cdf1a3180dc8b003bf87533
Alex <xrlogic1@gmail.com>
2023-02-08 21:44:58 +0400

    *

---
+++ Strings/Strings.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+
+
+int main()
+{
+	setlocale(0, "ru-ru");
+
+	//const double PI = 3.14;
+
+	/*double PI = 3.14;
+
+	PI = 3.212;*/
+
+	// const unsigned int value = 42;
+	char a = 72;
+	char b = 101;
+	char c = 108;
+	char d = 108;
+	char e = 111;
+
+	std::cout << a << b << c << d << e << '\n';
+
+
+	std::cout << "Hello\n\tC++\n";
+	std::cout << "Good\b\bbye\n";
+	std::cout << "Good\rbye\n";
+	std::cout << "\\\n";
+	std::cout << "\\\"";
+
+}
