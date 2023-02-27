3959a240a8dbc0798f4c4bd01e8d92ad71bbe232
Alex <xrlogic1@gmail.com>
2023-02-27 20:46:18 +0400

    *

---
+++ MultidimensionalArrays/MultidimensionalArrays.cpp
@@ -0,0 +1,84 @@
+#include <iostream>
+
+int main()
+{
+	//{
+	//    const int rows = 3;
+	//    const int cols = 3;
+
+	//    int arr[rows][cols]
+	//    {
+	//        //  0   1   2
+	//           {10, 42, 12}, // row 0
+	//           {99, 13, 15}, // row 1
+	//           {90, 44, 51}  // row 2
+	//    };
+
+	//    // show row
+	//    for (int j = 0; j < cols; ++j)
+	//    {
+	//        std::cout << arr[1][j] << '\n';
+	//    }
+
+	//    std::cout << "---------------\n";
+
+	//    for (int i = 0; i < rows; ++i)
+	//    {
+	//        std::cout << arr[i][1] << '\n';
+	//    }
+
+	//    for (int i = 0; i < rows; ++i)
+	//    {
+	//        std::cout << "{ ";
+	//        for (int j = 0; j < cols; ++j)
+	//        {
+	//            std::cout << arr[i][j];
+
+	//            if (j != cols - 1)
+	//            {
+	//                std::cout << ", ";
+	//            }
+	//        }
+	//        std::cout << " } \n";
+	//    }
+	//}
+
+	
+	/*{
+		char square[5][5]{
+		   {' ', '*', '*', '*', ' '},
+		   {'*', '*', '*', '*', '*'},
+		   {'*', '*', ' ', '*', '*'},
+		   {'*', '*', '*', '*', '*'},
+		   {' ', '*', '*', '*', ' '},
+		};
+
+		for (int i = 0; i < 5; ++i)
+		{
+			for (int j = 0; j < 5; ++j)
+			{
+				std::cout << square[i][j] << ' ';
+			}
+
+			std::cout << '\n';
+		}
+	}*/
+
+	{
+		const int rows = 3;
+		const int cols = 3;
+
+		int arr[rows][cols]
+		{
+			 // 0   1   2
+			   {10, 42, 12}, // 0
+			   {99, 13, 15}, // 1
+			   {90, 44, 51}  // 2
+		};
+
+		for (int i = 0; i < rows * cols; ++i)
+		{
+			std::cout << arr[0][i] << '\n';
+		}
+	}
+}
