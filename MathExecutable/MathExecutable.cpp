01ac11a864f151a89dc70d546349a9d88096d687
Alex <xrlogic1@gmail.com>
2023-05-08 21:03:37 +0400

    *

--- MathExecutable/MathExecutable.cpp
+++ MathExecutable/MathExecutable.cpp
@@ -1,13 +1,28 @@
 #include <iostream>
+#include <iomanip>
 #include "mymath.h"
 
 
 int main()
 {
-    std::cout << add(5, 10) << '\n';
-    std::cout << divide(15, 2) << '\n';
-    std::cout << mult(2, 3) << '\n';
-    std::cout << subtract(6, 15) << '\n';
+    //std::cout << add(5, 10) << '\n';
+    //std::cout << divide(15, 2) << '\n';
+    //std::cout << mult(2, 3) << '\n';
+    //std::cout << subtract(6, 15) << '\n';
+
+    float a = 0.1;
+    float b = 0.2;
+    std::cout << std::setprecision(10);
+    std::cout << a + b << '\n';
+
+    if (a + b == 0.3)
+    {
+        std::cout << "equals" << '\n';
+    }
+    else
+    {
+        std::cout << "not equals" << '\n';
+    }
 
     system("pause");
 }
